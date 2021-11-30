73d6c9992485bc15907dd7880d0cbb8aa9e1b72f
lisawaeger <80891762+lisawaeger@users.noreply.github.com>
2021-11-30 13:26:57 +0100

    copy

--- chat/index.js
+++ chat/index.js
@@ -1,66 +1,99 @@
+const socket = io('ws://localhost:8080');
+
+socket.on('message', text => {
+
+    const li = document.createElement('li');
+    li.innerHTML = text;
+    document.querySelector('ul').appendChild(el)
+
+});
+
+document.querySelector('button').onclick = () => {
+
+    const text = document.querySelector('input').value;
+    socket.emit('message', text)
+    
+}
+
 // class for sender
-class sender {
-    constructor(firstName, lastName,profilpicture, input) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.profilpicture = profilpicture
-      this.input = input;
-    }
-    details() 
-    { return this.firstName + " " + this.lastName;
-    }
-    fullname() {
-      return this.firstName + this.lastName;
-    }
+class user {
+  constructor(firstName, lastName, profilpicture, userID) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.profilpicture = profilpicture;
+    this.userID = userID;
   }
-
-    // class for receiver
-  class receiver {
-    constructor(firstName, lastName,profilpicture, input) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.profilpicture = profilpicture
-      this.input = input;
-    }
-    details() 
-    { return this.firstName + " " + this.lastName;
-    }
-    fullname() {
-      return this.firstName + this.lastName;
-    }
+  getDetails() {
+    return this.profilpicture + " " + this.userID + " " + this.firstName + " " + this.lastName;
   }
-
-     // class for message
-     class message{
-      constructor(userName, messageId, timeStamp, input) {
-        this.userName = userName;
-        this.messageId = messageId;
-        this.timeStamp = timeStamp
-        this.input = input;
-      }
-      directmessage() 
-      { return this.username + " : " + this.input + " " + this.timeStamp;
-      }
-      
-    }
+}
+// class for message
+class message extends user {
+  constructor(firstName, lastName,input, time) {
+    super(firstName,lastName);
+    this.input = input;
+    this.time = time;
+  }
   
-
-
   // Function for the time stamp
-
-  function getTime() {
+  getTime() {
     let today = new Date();
     hours = today.getHours();
     minutes = today.getMinutes();
 
     if (hours < 10) {
-        hours = "0" + hours;
+      hours = "0" + hours;
     }
 
     if (minutes < 10) {
-        minutes = "0" + minutes;
+      minutes = "0" + minutes;
     }
     let time = hours + ":" + minutes;
     return time;
+  }
+
+  //Function for message input
+  getInput() {
+    return this.firstName + this.lastName + this.getTime() + this.input;
+  }
 }
 
+//class for chat
+class chat extends message {
+  constructor(input, time, history, chatID) {
+    super(input, time);
+    this.history = history;
+    this.chatID = chatID;
+  }
+  //create chat
+  putChat() {
+    return this.getInput() + this.history + this.chatID;
+  }
+}
+
+
+// class for Two Person Chat
+class twoPersonChat extends chat, user {
+  constructor(firstName, lastName, profilpicture, userID, input, time, history, chatID, status) {
+    super(firstName, lastName, profilpicture, userID,input, time, history, chatID);
+    this.status = status;
+  }
+  //create twoPersonChat
+  puttwoPersonChat (){
+return this.getDetails() + this.getChat() + this.status;
+  }
+}
+
+
+// class for Groupchat
+class groupChat extends chat {
+  constructor(input, time, history, chatID, groupName, groupImage){
+    super(input, time, history, chatID)
+    this.groupName = groupName;
+    this.groupImage = groupImage;
+  }
+  //create groupChat 
+  putgroupChat(){
+   return this.getChat() + this.groupName + this.groupImage
+  }
+}
