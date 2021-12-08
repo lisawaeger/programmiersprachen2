528450b54915f922b655d77fd69ad38b95808f6a
lisawaeger <80891762+lisawaeger@users.noreply.github.com>
2021-12-08 03:28:31 +0100

    einbauen von vue

--- chat/server.js
+++ chat/server.js
@@ -1,18 +1,28 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html');});
+app.use('/style', express.static(__dirname+'/style'))
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
+  socket.username = 'anonymous';
+  socket.on('change username', (name) => socket.username = name)
+  socket.on('message', (msg) => io.emit('message',
+  { 'user': socket.username, 'message': msg }))
+  socket.on('join', (username) => {
+    if (username != null) {
+      socket.username = username
+    }
+    socket.broadcast.emit('message',
+    { 'user': 'Server', 'message': socket.username + ' has joined!'})
+  })
+})
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
---
+++ chat_/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Chat</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div id="app">
+      {{greeting}}
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
+  </body>
+</html>
