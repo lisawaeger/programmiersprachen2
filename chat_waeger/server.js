80a3c0c8be4f8354a828c09d6d424c1918cfa0f0
lisawaeger <80891762+lisawaeger@users.noreply.github.com>
2021-12-14 12:53:30 +0100

    Kommentare

--- chat_waeger/server.js
+++ chat_waeger/server.js
@@ -5,23 +5,29 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');});
+//bekommt html file
+res.sendFile(__dirname + '/index.html');});
+//bekommt css file
 app.use('/style', express.static(__dirname+'/style'))
 
 io.on('connection', (socket) => {
+  //username anfangs anonym falls jemand keinen namen angibt
   socket.username = 'anonym';
+  //username wird bei Eingabe eines Namens überschrieben
   socket.on('change username', (name) => socket.username = name)
+  //wenn user eine nachricht schreibt wird sie so weitergegeben usename: message
   socket.on('message', (msg) => io.emit('message',
   { 'user': socket.username, 'message': msg }))
 
   socket.on('join', (username) => {
     if (username != null) {
       socket.username = username 
+      //Willkommensnachricht !Problem soll nur bei user1 angezeigt werden!
       io.emit ('message',
    {'user': 'Server', 'message': 'Willkommen ' + socket.username + '!'
   })
     }
- 
+    //anderen usern wird angezeigt wenn jemand dem chat beitritt
     socket.broadcast.emit('message',
     { 'user': 'Server', 'message': socket.username + ' ist beigetreten!'})
   })
